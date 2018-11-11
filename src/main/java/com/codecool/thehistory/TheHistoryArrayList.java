2aa9f94d26b7261d6b514842ef679177a15cbd1a
boldizsarBalogh <bal.boldizsar@gmail.com>
2018-11-11 18:20:17 +0100

    Implemented the arraylist and linkedlist methods

--- src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
@@ -1,11 +1,18 @@
 package com.codecool.thehistory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class TheHistoryArrayList implements TheHistory {
+    private String format(String[] arr){
+        StringBuilder builder = new StringBuilder();
+        for (String value : arr) {
+            builder.append(value);
+            builder.append(" ");
+        }
+        if (builder.length() > 0) builder.deleteCharAt(builder.length() - 1);
+
+        return builder.toString();
+    }
     /**
      * This implementation should use a String ArrayList so don't change that!
      */
@@ -14,32 +21,52 @@ public class TheHistoryArrayList implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        wordsArrayList = Arrays.asList(text.split("\\s+"));
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        ListIterator iterator = wordsArrayList.listIterator();
+        while(iterator.hasNext()){
+            String word = (String)iterator.next();
+            if(wordToBeRemoved.equals(word)) {
+                iterator.set("");
+            }
+        }
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        final int size = wordsArrayList.size();
+        return size;
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        wordsArrayList = new ArrayList<>();
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+       for(int i =  0; i < wordsArrayList.size();i++){
+            if(wordsArrayList.get(i).equals(from)){
+                wordsArrayList.set(i,to);
+            }
+       }
     }
-
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
+        String fromString = format(fromWords);
+        String toString = format(toWords);
+        String text = toString();
+        String regex = "\\b" + fromString + "\\b";
+        String result = text.replaceAll(regex, toString);
+        add(result);
     }
 
     @Override
